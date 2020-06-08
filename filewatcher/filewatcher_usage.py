365408bc1d7b00d16bdc38a5b43d8e96c209bfb4
cyberinfy <kvrks@outlook.com>
2020-06-08 08:41:40 +0530

    Added filewatcher_usage.py

--- filewatcher/filewatcher_usage.py
+++ filewatcher/filewatcher_usage.py
@@ -5,10 +5,10 @@ import filewatcher
 
 
 # Retrieves files information from sub-directories too if recuresively=True
-files_info_dict = filewatcher.get_files_info(directory='/home/krishna/mygit-repos/',recursively=True)
+files_info_dict = filewatcher.get_files_info(directory='/home/krishna/mygit-repos/',filemask='*',recursively=True,sortbytime=False)
 
 # Doesn't retrieve files information from sub-directories if recursively=False
-files_info_dict = filewatcher.get_files_info(directory='/home/krishna/mygit-repos/',recursively=False)
+files_info_dict = filewatcher.get_files_info(directory='/home/krishna/mygit-repos/',filemask='*',recursively=False,sortbytime=True)
 
 for k,v in files_info_dict.items():
 	print('key:', k)
