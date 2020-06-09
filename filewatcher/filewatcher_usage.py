7455e780d88538d7e644699b205dbef27685d7be
Krishna <61433274+cyberinfy@users.noreply.github.com>
2020-06-10 00:21:23 +0530

    Update filewatcher_usage.py

--- filewatcher/filewatcher_usage.py
+++ filewatcher/filewatcher_usage.py
@@ -6,7 +6,7 @@ from filewatcher import getinfo
 '''
 getinfo     - Keyword Arguments:
 directory   - The location you want to capture files from
-filemask     - To specify file type, Use '*' to retrive info of all files
+filemask    - To specify file type, Use '*' to retrive info of all files
 recursively - Get files information from sub-directories too if value is True 
 sortbytime  - Sorts the file_info_dict based on last modified time
 showprint   - Prints a file_info object in console after each retrival if value is True
